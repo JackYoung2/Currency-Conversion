b54d2eadbd75d131774a08b6faf7cb8e0be9c012
Jack Young <37196656+JackYoung2@users.noreply.github.com>
2023-07-10 09:21:23 +0100

    Initial Commit

---
+++ CurrencyConversion/CurrencyConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConversionApp.swift
+//  CurrencyConversion
+//
+//  Created by Jack Young on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
